d58095059a4565a060a6e739a788a710e8af9a41
Shakibul Hasan <82836783+shakibulhasan1122@users.noreply.github.com>
2025-03-16 11:21:40 +0600

    Update urls.py

--- livechat/urls.py
+++ livechat/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chat/', include('chat.urls')),
+    path('', include('chat.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='chat/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='chat:index'), name='logout'),
     path('register/', chat_views.register, name='register'),
